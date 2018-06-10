b54879b2422e3d31143df74c61417717eabd9970
sameer <Sanjay.row.com@gmail.com>
2018-06-11 00:00:23 +0530

    ghjk

--- GitHubDemo/src/com/nt/git/MyMath.java
+++ GitHubDemo/src/com/nt/git/MyMath.java
@@ -3,14 +3,21 @@ package com.nt.git;
 public class MyMath {
 
 	public String sum() {
+		return null;
+
+	/*public String sum() {
+>>>>>>> stash
 		return "";
 		//samer is a,a good boy he aal rhe thimngs
 //		alff bjdy  cch nmhckh cmhkb  
 //		smj djkgbdjggbdjgjg dkjh dbjgd mb  dg
+<<<<<<< HEAD
 	}//hey Ram How Is Sita ?
 	public String Sum() {
 		return "";//sdfghjkl;luyktfdszxc
 	}
 
-
+=======
+	}*/
+	}
 }
